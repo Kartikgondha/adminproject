b4d8b2c3b2487b012f9cecd634d72f534e5622ec
Kartikgondha <kartikgondha2000@gmail.com>
2022-06-23 12:25:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './Component/Layout/Layout'
+
+function App() {
+  return (
+    <Layout >
+      <>
+      <h2>har har gange</h2>
+      </>
+    </Layout>
+  );
+}
+
+export default App;
